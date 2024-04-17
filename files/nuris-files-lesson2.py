a13c8cf5dda5f3bf14b37ba8a23a711a9cffa37e
nurislamkalmanbetov <nurislam_96@mail.ru>
2024-04-17 19:56:25 +0600

    add first commit

---
+++ files/nuris-files-lesson2.py
@@ -0,0 +1,31 @@
+# №1 Задача 
+
+# Напишите код, где открываете файл "my_name.txt" и запишите туда свое имя 
+# После напишите код, который читает этот файл 
+
+# with open("my_name.txt", "w") as file:
+#     file.write("Nurislam")
+
+# with open("my_name.txt", "r") as file:
+#     print(file.read())
+
+# _______________________________________________________________________________ 
+
+# №2 Задача 
+# Создать новый текстовый файл и записать в него список 5 имен.
+
+# names = ['Nurislam', 'Roman', 'Chika', 'Kanymet', 'Asker']
+
+# with open('frends.txt', 'w') as file:
+#     for name in names:
+#         file.write(name + '\n')
+
+
+# №3 Задача 
+# Добавить в этот же файл новое имя
+
+# names = ['Sayka']
+
+# with open('frends.txt', 'a') as file:
+#     for name in names:
+#         file.write(name + '\n')
